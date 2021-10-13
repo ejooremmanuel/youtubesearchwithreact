adef695a53a1f20830baaa061e6b98ec396228e0
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-10-13 17:20:41 +0100

    .env file

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-import env from "react-dotenv";
-const KEY = "AIzaSyDspX4KMKKi78duX0a7rDyXzXcLVf1ifTc";
 
 export default axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3",
@@ -8,6 +6,6 @@ export default axios.create({
     part: "snippet",
     maxResults: 4,
     type: "video",
-    key: KEY,
+    key: process.env.API_KEY,
   },
 });
